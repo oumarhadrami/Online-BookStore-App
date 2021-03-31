b5605c62d48136f16d9f037484a182dbccae3baa
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-03-31 14:22:20 +0000

    Change home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { BookListComponent } from './components/book-list/book-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'books', component: BookListComponent },
   { path: 'books/:id', component: BookDetailsComponent},
   { path: 'category/:id', component: BookListComponent },
   { path: 'search/:keyword', component: BookListComponent },
-  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: '', component: BookListComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
